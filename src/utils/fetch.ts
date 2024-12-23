8028f180c993474b2557c80cdf9baaf554ecd3ef
violetbee <caglarkarahuseyin28@gmail.com>
2024-12-23 18:07:44 +0300

    envfix

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
