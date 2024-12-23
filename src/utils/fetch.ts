e3ad15eb842bffc85c0606e8ab779e499e31bbc7
violetbee <caglarkarahuseyin28@gmail.com>
2024-12-23 18:12:26 +0300

    env fix

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_JWT_SECRET,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
